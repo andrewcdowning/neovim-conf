7e748c02f7084a5b66fdd6fdcc74d38967ec5a2f
Andrew Downing <andrew.c.downing@gmail.com>
2024-02-08 16:23:09 -0700

    adding completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -21,8 +21,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        capabilites = capabilities
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
