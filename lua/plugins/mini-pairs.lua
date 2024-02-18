eae2cafdcd67a72a82063633360e6b47b4571953
Andrew Downing <andrew.c.downing@gmail.com>
2024-02-17 19:39:58 -0700

    bufferlines, pairs, none

---
+++ lua/plugins/mini-pairs.lua
@@ -0,0 +1,8 @@
+return {
+	 "echasnovski/mini.nvim", version = "*",
+  config = function()
+    local mini = require("mini.pairs")
+    mini.setup()
+  end
+}
+
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,16 +6,12 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.completion.spell,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.diagnostics.staticcheck,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
