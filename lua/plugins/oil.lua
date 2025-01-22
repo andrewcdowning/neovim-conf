cd9a44984dc5d71b1d3a181292e7fd1f39a56c4f
Andrew Downing <downandr@amazon.com>
2025-01-22 23:33:56 +0000

    add: harpoon + oil keymap

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,6 +7,7 @@ return {
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
+  vim.keymap.set("n", "<leader>0", "<CMD>Oil<CR>", {desc = "Open parent directory"})
   }
 }
